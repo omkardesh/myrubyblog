29459fc37c6ff3befa0878ab708385457056158c
Omkar Deshpande <omkard199@gmail.com>
2019-06-25 22:29:23 +0530

    Updating after listing the database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,14 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def index
+		@allarticles = Article.all	
+	end
+
+	def edit
+		@article = Article.find(params[:id])	
+	end
+	
 	def create
 		
 		@article = Article.new(article_params)
@@ -12,8 +20,17 @@ class ArticlesController < ApplicationController
 			redirect_to article_path(@article)	
 		else
 			render 'new'
+		end		
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article is updated successfully"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
 		end
-			
 	end
 
 	def show
