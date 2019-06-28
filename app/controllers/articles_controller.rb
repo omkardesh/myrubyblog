420942a28e456414a86fc2827b4a7b7e59241ada
Omkar Deshpande <omkard199@gmail.com>
2019-06-28 13:03:29 +0530

    Msgs styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 		
 		@article = Article.new(article_params)
 		if @article.save
-			flash[:notice] = "Article was successfully created"
+			flash[:success] = "Article was successfully created"
 			redirect_to article_path(@article)	
 		else
 			render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
 		
 	        # @article = Article.find(params[:id])
 		if @article.update(article_params)
-			flash[:notice] = "Article is updated successfully"
+			flash[:success] = "Article is updated successfully"
 			redirect_to article_path(@article)
 		else
 			render 'edit'
@@ -43,7 +43,7 @@ class ArticlesController < ApplicationController
 	def destroy
 		# @article = Article.find(params[:id])
 		@article.destroy
-		flash[:notice]="Article was successfully deleted"
+		flash[:danger]="Article was successfully deleted"
 		redirect_to articles_path
 	end
 
