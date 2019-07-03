accb13aec8da3ac3c7528fa57edf62e07874a800
Omkar Deshpande <omkard199@gmail.com>
2019-07-03 12:18:57 +0530

    add secure password and attribute to users

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '~> 5.2.3'
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
+gem 'bcrypt', '~> 3.1.7'
 
 gem 'bootstrap-sass', '~> 3.4.1'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_02_092854) do
+ActiveRecord::Schema.define(version: 2019_07_03_064417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_092854) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
