6fc554da35bc4cc0ab6f29ade01adb357d24a836
Omkar Deshpande <omkard199@gmail.com>
2019-07-06 13:22:16 +0530

    Admin user added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_03_064417) do
+ActiveRecord::Schema.define(version: 2019_07_06_112024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_07_03_064417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
